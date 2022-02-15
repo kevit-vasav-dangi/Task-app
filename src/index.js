97e805d38a219bddd492167d06b0dde27bac6464
vasav <vasav.dangi@kevit.io>
2022-02-15 16:05:52 +0530

    section12

--- src/index.js
+++ src/index.js
@@ -3,17 +3,51 @@ require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task');
 const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 const e = require('express');
 
 
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req,res,next)=>{
+//     // console.log(req.method,req.path);
+//     // next()
+//     if(req.method === 'GET'){
+//         res.send('GET is not acceptable')
+//     }else{
+//         next()
+//     }
+// })
+app.use((req,res,next)=>{
+    if(req.method){
+        res.status(503).send('site is under maintainance')
+    }else{
+        next()
+    }
+})
 
 
 app.use(express.json())
 app.use(userRouter)
+app.use(taskRouter);
 
 app.listen(port,()=>{
     console.log('server is ready'+port);
 })
+const jwt = require('jsonwebtoken')
+//const bcrypt = require('bcrypt')
+const myFunction = async ()=>{
+    // const password = '1234vdas!';
+    // const hashPassword = await bcrypt.hash(password,8)
+    // console.log(password);
+    // console.log(hashPassword);
+    // const isMatch = await bcrypt.compare('1234vdas!',hashPassword)
+    // console.log(isMatch);
+    const token = jwt.sign({_id:'abc123'}, 'this is my new course',{expiresIn:'7 days'})
+
+    console.log(token);
+    const data =jwt.verify(token,'this is my new course')
+    console.log(data);
+}
+myFunction()
--- src/models/user.js
+++ src/models/user.js
@@ -1,47 +1,86 @@
 const mongoose=require('mongoose')
 const validator=require('validator');
+const bcrypt = require('bcrypt')
+const jwt=require('jsonwebtoken')
 //mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     // useNewUrlParser:true,
     // useCreateIndex:true
 //})
- const User = mongoose.model('User',{
-     name:{
-         type:String,
-         required:true,
-         trim:true
-     },
-     email:{
-         type:String,
-         required:true,
-         trim:true,
-         validate(value){
-             if(!validator.isEmail(value)){
-                 throw new Error('email is invalid')
-             }
-         }
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true,
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('email is invalid')
+            }
+        }
 
-     },
-     age: {
-         type : Number,
-         default : 10,
-         validate(value){
-             if(value<0){
-             throw new Error('age must be positive number')
-         }
-         }
-     },
-     password:{
-         type:String,
-         required:true,
-         trim:true,
-         minlength:7,
-         validate(value){
-              if(value.toLowerCase().includes('password')){
-                 throw new Error('password cant be set as password')
-             }
-         }
-     }
- })
+    },
+    age: {
+        type : Number,
+        default : 10,
+        validate(value){
+            if(value<0){
+            throw new Error('age must be positive number')
+        }
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength:7,
+        validate(value){
+             if(value.toLowerCase().includes('password')){
+                throw new Error('password cant be set as password')
+            }
+        }
+    },
+    tokens:[{
+        token: {
+            type:String,
+            required:true
+        }
+    }]
+})
+userSchema.methods.generateAuthToken = async function(){
+    const user = this
+    const token=jwt.sign({_id:user._id.toString()},'this is my new course')
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
+}
+userSchema.statics.findByCredentials = async(email,password)=>{
+    const user = await User.findOne({ email})
+    if(!user){
+        throw new Error('unable to login')
+    }
+    const isMatch = await bcrypt.compare(password,user.password)
+    if(!isMatch){
+        throw new Error('unable to login')
+    }
+    return user
+}
+
+userSchema.pre('save',async function (next){
+    const user = this 
+
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password,8)
+    }
+    //console.log(('just before saving!'));
+
+    next()
+})
+ const User = mongoose.model('User',userSchema)
 // const me = new User({
 //      name:'    dp',
 //      age:16,
