e205859329ef94900a31c2bec3af90f22136f9b0
vasav <vasav.dangi@kevit.io>
2022-02-16 16:00:31 +0530

    authentication done in user

--- src/index.js
+++ src/index.js
@@ -20,13 +20,13 @@ const port = process.env.PORT || 3000
 //         next()
 //     }
 // })
-app.use((req,res,next)=>{
-    if(req.method){
-        res.status(503).send('site is under maintainance')
-    }else{
-        next()
-    }
-})
+// app.use((req,res,next)=>{
+//     if(req.method){
+//         res.status(503).send('site is under maintainance')
+//     }else{
+//         next()
+//     }
+// })
 
 
 app.use(express.json())
@@ -36,19 +36,24 @@ app.use(taskRouter);
 app.listen(port,()=>{
     console.log('server is ready'+port);
 })
-const jwt = require('jsonwebtoken')
-//const bcrypt = require('bcrypt')
-const myFunction = async ()=>{
-    // const password = '1234vdas!';
-    // const hashPassword = await bcrypt.hash(password,8)
-    // console.log(password);
-    // console.log(hashPassword);
-    // const isMatch = await bcrypt.compare('1234vdas!',hashPassword)
-    // console.log(isMatch);
-    const token = jwt.sign({_id:'abc123'}, 'this is my new course',{expiresIn:'7 days'})
+// const jwt = require('jsonwebtoken')
+// //const bcrypt = require('bcrypt')
+// const myFunction = async ()=>{
+//     // const password = '1234vdas!';
+//     // const hashPassword = await bcrypt.hash(password,8)
+//     // console.log(password);
+//     // console.log(hashPassword);
+//     // const isMatch = await bcrypt.compare('1234vdas!',hashPassword)
+//     // console.log(isMatch);
+//     const token = jwt.sign({_id:'abc123'}, 'this is my new course',{expiresIn:'7 days'})
 
-    console.log(token);
-    const data =jwt.verify(token,'this is my new course')
-    console.log(data);
+//     console.log(token);
+//     const data =jwt.verify(token,'this is my new course')
+//     console.log(data);
+// }
+// myFunction()
+const main = async()=>{
+    const task = await Task.findById('620cd0650ddeddc9586be0f5')
+    console.log(task.owner);
 }
-myFunction()
+main()
--- src/models/task.js
+++ src/models/task.js
@@ -14,6 +14,10 @@ const Task = mongoose.model('Tasks',{
     completed : {
         type : Boolean,
         default:false
+    },
+    owner : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true
     }
 })
 // const newTask= new Task({
